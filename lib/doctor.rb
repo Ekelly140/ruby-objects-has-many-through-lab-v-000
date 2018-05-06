50a39a8eec1c4dcd921f02ce1ea9f04925f1f867
ERIC KELLY <eakelly220@gmail.com>
2018-05-06 21:06:58 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,8 +1,20 @@
 class Doctor 
   attr_accessor :name 
-  attr_reader:appointment
+  attr_reader:appointments
   
+  def initialize(name)
+    @name = name 
+    @appointments = []
+  end 
   
+  def add_appointment(appointment) 
+    appointment.doctor(self)
+    @appointments << appointment
+  end 
+  
+  def patients
+    @appointments.map {|app| app.patient }
+  end 
   
   
   
